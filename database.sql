05b8d9377b79e3d8e77284035ad4f344270e7b08
boshiro123 <b.shiroki@mail.ru>
2024-04-10 20:41:50 +0300

    new sql file

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 CREATE SCHEMA bot;
 
-CREATE TABLE bot.orders (
+CREATE TABLE bot.orders(
   id UUID NOT NULL,
    chatid BIGINT NOT NULL,
    type_of_order VARCHAR(255),
    type_of_work VARCHAR(255),
-   duration date,
+   duration VARCHAR(255),
    subject VARCHAR(255),
    decsriptons VARCHAR(255),
    price INTEGER NOT NULL,
@@ -22,8 +22,3 @@ CREATE TABLE bot.users (
    role VARCHAR(255),
    CONSTRAINT pk_users PRIMARY KEY (id)
 );
-
-INSERT INTO bot.users (id, user_name, name, register_at, role)
-VALUES (733743450, 'boshiro_123', 'Богдан', '2024-03-20 14:12:00.152', 'admin');
-INSERT INTO bot.users (id, user_name, name, register_at, role)
-VALUES (589475531, 'Eug212132', 'Eugene', '2024-03-24 22:27:17.323', 'admin');
